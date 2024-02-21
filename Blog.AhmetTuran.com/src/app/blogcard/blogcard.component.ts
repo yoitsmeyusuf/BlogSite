42775e0ad33374a9c3b64b7d1893a18cb26a5d6a
Oğuzhan Altunbey <oguzaltnby@protonmail.com>
2024-02-21 15:54:41 +0300

    a

--- Blog.AhmetTuran.com/src/app/blogcard/blogcard.component.ts
+++ Blog.AhmetTuran.com/src/app/blogcard/blogcard.component.ts
@@ -8,47 +8,70 @@ import { RouterModule } from '@angular/router';
   selector: 'app-blog',
   templateUrl: 'blogcard.component.html',
   styleUrls: ['./blogcard.component.scss'],
-  imports: [CommonModule, RouterModule] 
+  imports: [CommonModule, RouterModule]
 })
 export class CardComponent implements OnInit {
-  blogs : any = [];
-  blogsrecent : any = [];
+  blogs: any = [];
+  blogsrecent: any = [];
 
   constructor(private blogService: BlogService) { }
 
   ngOnInit(): void {
-    
+
     this.blogService.getBlogs().subscribe(blogs => {
       //make blogs content only 80 characters and add ... to the end
-       // if category is 0 its string Teknoloji, if its 1 its string Bilim, if its 2 its string işletme, if its 3 its string Diğer
+      // if category is 0 its string Teknoloji, if its 1 its string Bilim, if its 2 its string işletme, if its 3 its string Diğer
       blogs.forEach((blog: any) => {
-        if(blog.category == 0){
+        if (blog.category == 0) {
           blog.category = "Teknoloji";
         }
-        else if(blog.category == 1){
+        else if (blog.category == 1) {
           blog.category = "Bilim";
         }
-        else if(blog.category == 2){
+        else if (blog.category == 2) {
           blog.category = "İşletme";
         }
-        else if(blog.category == 3){
+        else if (blog.category == 3) {
           blog.category = "Diğer";
-        }});
+        }
+      });
       blogs.forEach((blog: any) => {
-        if(blog.content.length > 300){
+        if (blog.content.length > 300) {
           blog.content = blog.content.substring(0, 300) + "...";
         }
       });
       //the times format 2024-02-19T00:00:00.000Z to 2024-02-19
-      blogs.forEach((blog: any) => {
-        blog.publishDate = blog.publishDate.split("T")[0];
+      // Ay isimlerini içeren bir dizi oluştur
+      const months = [
+        "Ocak",
+        "Şubat",
+        "Mart",
+        "Nisan",
+        "Mayıs",
+        "Haziran",
+        "Temmuz",
+        "Ağustos",
+        "Eylül",
+        "Ekim",
+        "Kasım",
+        "Aralık"
+      ];
+
+      // Blogları döngüyle işle ve her birinin yayın tarihini güncelle
+      blogs.forEach((blog) => {
+        // Yayın tarihini "T" karakterine göre ayır ve yıl, ay ve gün kısımlarını al
+        const [year, month, day] = blog.publishDate.split("T")[0].split("-");
+
+        // Ayı sayıdan isme dönüştür ve blog nesnesine kaydet
+        blog.publishDate = `${parseInt(day)} ${months[parseInt(month) - 1]} ${year}`;
       });
+
       this.blogs = blogs;
     });
-   
-   
+
+
 
   }
-  
+
 
 }
