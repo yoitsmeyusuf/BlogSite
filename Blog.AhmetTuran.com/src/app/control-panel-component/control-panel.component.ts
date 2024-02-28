d2406193a3731b7726efc28c99671868d622f5b6
yoitsmeyusuf <muhammetyusufkaydi@gmail.com>
2024-02-28 14:45:05 +0300

    richtexteditor configuration

--- Blog.AhmetTuran.com/src/app/control-panel-component/control-panel.component.ts
+++ Blog.AhmetTuran.com/src/app/control-panel-component/control-panel.component.ts
@@ -26,33 +26,42 @@ export class ControlPanelComponent {
 
   model = this.blog;
   //write me an confguration for quill editor
+  quill: any;
+
   quillConfig = {
-    toolbar: [
-      ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
-      ['blockquote', 'code-block'],
-      [{ 'header': 1 }, { 'header': 2 }],               // custom button values
-      [{ 'list': 'ordered'}, { 'list': 'bullet' }],
-      [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
-      [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
-      [{ 'direction': 'rtl' }],                         // text direction
-      [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
-      [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
-      [{ 'color': [] }],          // dropdown with defaults from theme
-      [{ 'font': [] }],
-      [{ 'align': [] }],
-      ['clean'],                                         // remove formatting button
-      ['link', 'image', 'video']         // link and image, video
-    ],
-    handlers: {
-      
-    }
-    
+    modules: {
+      toolbar: [
+        ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+        ['blockquote', 'code-block'],
+  
+        [{ 'header': 1 }, { 'header': 2 }],               // custom button values
+        [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+        [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
+        [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
+        [{ 'direction': 'rtl' }],                         // text direction
+  
+        [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+        [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+  
+        [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+        [{ 'font': [] }],
+        [{ 'align': [] }],
+  
+        ['clean'],                                        // remove formatting button
+        ['link', 'image', 'video']                        // link and image, video
+      ]
+    },
+    theme: 'snow'  // or 'bubble'
   };
   //how can I get the images from quil editor
-  
+  // how to use quillconfig at quill
+
   constructor(private blogService: BlogService) { }
 
-  
+  // how to use quillconfig at quill
+  ngAfterViewInit() {
+    this.quill = new Quill('#editor', this.quillConfig);
+  }
   async createBlog() {
     
     let newcon = this.processHtmlContent(this.blog.content);
