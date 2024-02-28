76ebbc84fdfe0801077056f41e3816b808dd07b4
Oğuzhan Altunbey <oguzaltnby@protonmail.com>
2024-02-28 14:52:30 +0300

    some changes

--- Blog.AhmetTuran.com/src/app/control-panel-component/control-panel.component.ts
+++ Blog.AhmetTuran.com/src/app/control-panel-component/control-panel.component.ts
@@ -23,12 +23,13 @@ export class ControlPanelComponent {
     publishDate: '',
     imageURL: ''
   };
-
+  
   model = this.blog;
   //write me an confguration for quill editor
   quill: any;
 
   quillConfig = {
+<<<<<<< Updated upstream
     modules: {
       toolbar: [
         ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
@@ -57,6 +58,35 @@ export class ControlPanelComponent {
   // how to use quillconfig at quill
 
   constructor(private blogService: BlogService) { }
+=======
+    theme: 'snow',
+    toolbar: [
+      ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+      ['blockquote', 'code-block'],
+      [{ 'header': 1 }, { 'header': 2 }],               // custom button values
+      [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+      [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
+      [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
+      [{ 'direction': 'rtl' }],                         // text direction
+      [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+      [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+      [{ 'color': [] }],          // dropdown with defaults from theme
+      [{ 'font': [] }],
+      [{ 'align': [] }],
+      ['clean'],                                         // remove formatting button
+      ['link', 'image', 'video']         // link and image, video
+    ],
+    handlers: {
+      
+    }
+    
+  };
+  //how can I get the images from quil editor
+  
+  constructor(private blogService: BlogService) {
+    
+   }
+>>>>>>> Stashed changes
 
   // how to use quillconfig at quill
   ngAfterViewInit() {
