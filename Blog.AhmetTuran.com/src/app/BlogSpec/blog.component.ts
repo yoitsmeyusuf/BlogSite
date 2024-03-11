f0b29c05fdaf0cc768ae0bcbd437e82922788dbe
Oğuzhan Altunbey <oguzaltnby@protonmail.com>
2024-03-11 09:29:36 +0300

    css
    css

--- Blog.AhmetTuran.com/src/app/BlogSpec/blog.component.ts
+++ Blog.AhmetTuran.com/src/app/BlogSpec/blog.component.ts
@@ -11,12 +11,37 @@ import { BlogService } from '../blog.services';
 })
 export class BlogComponent implements OnInit {
   blogs : any = [];
+  blogsrecent: any = [];
+
 
   constructor(private blogService: BlogService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.route.params.subscribe(params => {
       this.blogService.getBlog(params['id']).subscribe(blog => {
+        const months = [
+          "Ocak",
+          "Şubat",
+          "Mart",
+          "Nisan",
+          "Mayıs",
+          "Haziran",
+          "Temmuz",
+          "Ağustos",
+          "Eylül",
+          "Ekim",
+          "Kasım",
+          "Aralık"
+        ];
+  
+        // Blogları döngüyle işle ve her birinin yayın tarihini güncelle
+        
+          // Yayın tarihini "T" karakterine göre ayır ve yıl, ay ve gün kısımlarını al
+          const [year, month, day] = blog.publishDate.split("T")[0].split("-");
+  
+          // Ayı sayıdan isme dönüştür ve blog nesnesine kaydet
+          blog.publishDate = `${parseInt(day)} ${months[parseInt(month) - 1]} ${year}`;
+        
         this.blogs = blog;
       });
     });
